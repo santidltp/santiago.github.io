d948ac927b33389c1b77af345e3d922c0fba6aa6
delats <santiago.delatorre@emc.com>
2015-11-04 18:52:47 -0500

    modiefied experience

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 (function(){
 	var portApp = angular.module('portApp',['ngRoute']);
-
-	portApp.controller('infoController',['$scope', '$http', function($scope,$http){
 	console.log("pollo")
-		
+	portApp.controller('experienceCTRL',['$scope', '$http', function($scope,$http){
 		$http.get('experience.json').success(function(data){
 			$scope.experience = data;
-		});
+		})
 	}]);
 })();
