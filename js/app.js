8d42b3a8041f7ef6e1c751da13bcdc0fca8f9803
delats <santiago.delatorre@emc.com>
2015-11-04 20:47:24 -0500

    fixed typos in technologies.json

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 (function(){
 	var portApp = angular.module('portApp',['ngRoute']);
-	var pollo="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/experience.json"
-		
-		portApp.controller('experienceCTRL',['$scope', '$http', function($scope,$http){
-		$http.get(pollo).success(function(data){
+	var xp="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/experience.json"
+	var tk="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/technologies.json"
+	
+	portApp.controller('experienceCTRL',['$scope', '$http', function($scope,$http){
+		$http.get(xp).success(function(data){
 			$scope.experience = data;
-			console.log(data)
 		})
 	}]);
-		portApp.controller('tech',['$scope','$html', function($scope,$html){
-			
-		}]);
+
+	portApp.controller('tech',['$scope','$html', function($scope,$html){
+		$http.get(tk).success(function(data){
+			$scope.technology = data;
+		});
+	}]);
+
 })();
