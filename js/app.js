38db3f14c7dc36abe1122198ba8c6f3dc9abad08
delats <santiago.delatorre@emc.com>
2015-11-10 21:27:26 -0500

    changed the project icons into AngularJS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 	var portApp = angular.module('portApp',['ngRoute']);
 	var xp="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/experience.json"
 	var tk="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/technologies.json"
+	var ic="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/proIcons.json"
 	
 	portApp.controller('experienceCTRL',['$scope', '$http', function($scope,$http){
 		$http.get(xp).success(function(data){
@@ -12,6 +13,12 @@
 	portApp.controller('techCTRL',['$scope', '$http', function($scope,$http){
 		$http.get(tk).success(function(data){
 			$scope.technology = data;
+		});
+	}]);
+
+	portApp.controller('icoCTRL',['$scope', '$http', function($scope,$http){
+		$http.get(ic).success(function(data){
+			$scope.icons = data;
 		});
 	}]);
 
