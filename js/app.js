3f8c736e50f14cd57bb18f1b2a5a23b3bf7b9ed1
delats <santiago.delatorre@emc.com>
2015-11-11 00:05:41 -0500

    separated json from js into its own folder

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 (function(){
 	var portApp = angular.module('portApp',['ngRoute']);
-	var xp="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/experience.json"
-	var tk="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/technologies.json"
-	var ic="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/proIcons.json"
-	var pj="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/js/proInfo.json"
+	var xp="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/json/experience.json"
+	var tk="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/json/technologies.json"
+	var ic="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/json/proIcons.json"
+	var pj="https://raw.githubusercontent.com/santidltp/santidltp.github.io/AngularJS/json/proInfo.json"
 	
 	portApp.controller('experienceCTRL',['$scope', '$http', function($scope,$http){
 		$http.get(xp).success(function(data){
